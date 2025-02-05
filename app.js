67647934e724fba1b3da7bc02ff2897183244038
jkhiba2023 <khan.jabir1055@gmail.com>
2025-02-05 19:27:53 +0530

    Added the turns and counts for per players

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function () {
@@ -15,6 +15,8 @@ p1Button.addEventListener('click', function () {
     p1Score += 1;
     if (p1Score === winningScore) {
       isGameOver = true;
+      p1Display.classList.add('winner');
+      p2Display.classList.add('loser');
     }
     p1Display.textContent = p1Score;
   }
@@ -24,8 +26,8 @@ p2Button.addEventListener('click', function () {
     p2Score += 1;
     if (p2Score === winningScore) {
       isGameOver = true;
-      p1Display.classList.add('winner');
-      p2Display.classList.add('loser');
+      p2Display.classList.add('winner');
+      p1Display.classList.add('loser');
     }
     p2Display.textContent = p2Score;
   }
